add5861dd1f88143136c91e38b4c776c63e0d3d0
Marco <marco.beer93@web.de>
2022-05-12 19:47:55 +0200

    added some basic classes

--- js/game.js
+++ js/game.js
@@ -1,11 +1,9 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new Character();
 
 function init() {
     canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
-    character.src = '../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png';
+    ctx = canvas.getContext('2d');
 
-    ctx.drawImage(character, 20, 20, 50, 150)
 }
