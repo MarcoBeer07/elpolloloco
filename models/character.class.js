6c43e9c346eecd21c7e2803982eb9c616fab584d
Marco <marco.beer93@web.de>
2022-05-13 18:12:27 +0200

    added keypress functions for moving character

--- models/character.class.js
+++ models/character.class.js
@@ -10,6 +10,7 @@ class Character extends MovableObject {
         'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png',
         'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png'
     ];
+    world;
 
     constructor() {
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png')
@@ -21,10 +22,14 @@ class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 5 % 6; => 0, Rest 5 // i= 0,1,2,3,4,5,6 repeat 0,1,2,3,4,5,6,...
-            let path = this.IMAGES_WALKING[i]; //loading image from IMAGES_WALKING on point 0
-            this.img = this.imageCache[path];
-            this.currentImage++;
+
+            if (this.world.keyboard.RIGHT) {
+                let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 5 % 6; => 0, Rest 5 // i= 0,1,2,3,4,5,6 repeat 0,1,2,3,4,5,6,...
+                let path = this.IMAGES_WALKING[i]; //loading image from IMAGES_WALKING on point 0
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+
         }, 8000 / 60)
 
     }
